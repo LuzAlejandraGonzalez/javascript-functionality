482391c415fb121f466867871b7e9140c4530e9d
LuzAlejandraGonzalez <lgonzalez@unitedvirtualities.com>
2019-12-18 16:42:29 -0700

    Task 3 and 4 of jsorg

--- jsorg/js/carousel.js
+++ jsorg/js/carousel.js
@@ -1,41 +1,64 @@
-$(document).ready(function(){
+var Carousel = (function() {
+    var $content;
+    var $items;
+    var $left;
+    var $right;
+    var contentWidth;
+    var itemsWidth;
+    var position;
+    var maxPosition;
 
-	function scrollLeft(evt) {
-		evt.preventDefault();
-		evt.stopPropagation();
-		evt.stopImmediatePropagation();
-
-		if (position > 0) {
-			position = Math.max(0,position - 250);
-		}
-
-		$items.css({ left: (-position) + "px" });
-	}
-
-	function scrollRight(evt){
-		evt.preventDefault();
-		evt.stopPropagation();
-		evt.stopImmediatePropagation();
-
-		if (position < maxPosition) {
-			position = Math.min(maxPosition,position + 250);
-		}
-
-		$items.css({ left: (-position) + "px" });
-	}
-
-	var $content = $("[rel=js-carousel] > [rel=js-content]");
-	var $items = $content.children("[rel=js-items]");
-	var $left = $("[rel=js-carousel] > [rel=js-controls] > [rel=js-left]");
-	var $right = $("[rel=js-carousel] > [rel=js-controls] > [rel=js-right]");
+    EVT.on("init", init);
 
 
-	var contentWidth = $content.width();
-	var itemsWidth = $items.width();
-	var position = 0;
-	var maxPosition = (itemsWidth - contentWidth);
+    function init() {
+        $content = $("[rel=js-carousel] > [rel=js-content]");
+        $items = $content.children("[rel=js-items]");
+        $left = $("[rel=js-carousel] > [rel=js-controls] > [rel=js-left]");
+        $right = $("[rel=js-carousel] > [rel=js-controls] > [rel=js-right]");
 
-	$left.on("click",scrollLeft);
-	$right.on("click",scrollRight);
 
-});
+        contentWidth = $content.width();
+        itemsWidth = $items.width();
+        position = 0;
+        maxPosition = (itemsWidth - contentWidth);
+
+        $left.on("click", scrollLeft);
+        $right.on("click", scrollRight);
+
+        $items.on("click", "[rel*='js-item-']", clickPerson);
+    }
+
+    function clickPerson(evt) {
+        var id = $(evt.target).attr("rel").replace(/^.*(\d+)$/, "$1");
+
+        EVT.emit("person-selected", id);
+    }
+
+
+    function scrollLeft(evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+        evt.stopImmediatePropagation();
+
+        if (position > 0) {
+            position = Math.max(0, position - 250);
+        }
+
+        $items.css({ left: (-position) + "px" });
+    }
+
+    function scrollRight(evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+        evt.stopImmediatePropagation();
+
+        if (position < maxPosition) {
+            position = Math.min(maxPosition, position + 250);
+        }
+
+        $items.css({ left: (-position) + "px" });
+    }
+
+
+})();
--- jsorg/js/details.js
+++ jsorg/js/details.js
@@ -1,25 +1,27 @@
-$(document).ready(function(){
+var Details = (function() {
+    var $content;
 
-	function loadPerson(evt){
-		var ID = $(evt.target).attr("rel").replace(/^.*(\d+)$/,"$1");
-		$.ajax("details/" + ID + ".html", {dataType: "text"})
-		.then(function(contents){
-			$content.html(contents);
-		});
-	}
+    EVT.on("init", init);
 
 
-	var $items = $("[rel=js-carousel] > [rel=js-content] > [rel=js-items]");
-	var $content = $("[rel=js-details]");
+    function init() {
+        $content = $("[rel=js-details]");
 
-	$items.on("click", "[rel*='js-item-']",loadPerson);
-	// on click of a carousel item, do an Ajax request for
-	// the "details/2.html" (or whatever) file for the person
-	// clicked, and load those contents into the `$content` div.
+        $content.on("click", "[rel=js-select-person]", selectPerson);
 
-	// hint: you will probably want to use "event propagation"
-	// (aka "event delegation"), by attaching a single event
-	// handler the `$content` element rather than individual
-	// event handlers to each item in the carousel.
+        EVT.on("person-selected", personClicked);
+    }
 
-});
+    function personClicked(id) {
+        $.ajax("details/" + id + ".html", { dataType: "text" }).then(function(contents) {
+            $content.html(contents);
+        });
+    }
+
+    function selectPerson(evt) {
+        evt.preventDefault();
+        var id = $(evt.target).attr("data-person");
+        EVT.emit("person-selected", id);
+    }
+
+})();
